d118919e832e3352ad06616fb9b41cb08d6c6c28
구홍모 <khm9990503@gmail.com>
2023-04-03 12:59:04 +0900

    2023

---
+++ ch01/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>Ch01.Hello vue</h3>
+  <p>hello wolrd</p>
+</template>
+
+<script>
+/*
+* 날짜 : 2023/04/03
+* 이름 : 구홍모
+* 내용 : Ch01. HelloVue 실습
+*
+* 환경 설정
+* 1) node.js 설치
+* 2) VSCode 확장팩 설치
+* - volor. vue3 snippets, prettier, ESLint
+* 프로젝트 생성
+* - npm create ch01
+* - cd ch01
+* - npm run serve
+*
+*/
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
