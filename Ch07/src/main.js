5ca3f73843606a3b7dea7eca63910cb13e06e3d3
구홍모 <khm9990503@gmail.com>
2023-04-19 13:52:22 +0900

    2023/04/19

--- Ch07/src/main.js
+++ Ch07/src/main.js
@@ -2,4 +2,5 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./routers/index.js";
-createApp(App).use(router).mount("#app");
+import store from "./store/index.js";
+createApp(App).use(router).use(store).mount("#app");
--- Ch08/src/components/sub2/Grids.vue
+++ Ch08/src/components/sub2/Grids.vue
@@ -1,54 +1,62 @@
 <template>
-  <h3>Grids</h3>
-
-  <h4>그리드 기본</h4>
-
+  <h4>Grids</h4>
   <v-container fluid>
     <v-row>
-      <v-col class="border text-center" cols="12">12</v-col>
+      <v-col cols="12" class="border text-center">12</v-col>
     </v-row>
     <v-row class="mt-4">
-      <v-col class="border text-center" cols="6">6</v-col>
-      <v-col class="border text-center" cols="6">6</v-col>
+      <v-col cols="6" class="border text-center">6</v-col>
+      <v-col cols="6" class="border text-center">6</v-col>
     </v-row>
     <v-row class="mt-4">
-      <v-col class="border text-center" cols="4">4</v-col>
-      <v-col class="border text-center" cols="4">4</v-col>
-      <v-col class="border text-center" cols="4">4</v-col>
+      <v-col cols="4" class="border text-center">4</v-col>
+      <v-col cols="4" class="border text-center">4</v-col>
+      <v-col cols="4" class="border text-center">4</v-col>
     </v-row>
     <v-row class="mt-4">
-      <v-col class="border text-center" cols="3">3</v-col>
-      <v-col class="border text-center" cols="3">3</v-col>
-      <v-col class="border text-center" cols="3">3</v-col>
-      <v-col class="border text-center" cols="3">3</v-col>
+      <v-col cols="3" class="border text-center">3</v-col>
+      <v-col cols="3" class="border text-center">3</v-col>
+      <v-col cols="3" class="border text-center">3</v-col>
+      <v-col cols="3" class="border text-center">3</v-col>
     </v-row>
     <v-row class="mt-4">
-      <v-col class="border text-center" cols="5">5</v-col>
-      <v-col class="border text-center" cols="5">5</v-col>
-      <v-col class="border text-center" cols="2">2</v-col>
+      <v-col cols="5" class="border text-center">5</v-col>
+      <v-col cols="5" class="border text-center">5</v-col>
+      <v-col cols="2" class="border text-center">2</v-col>
     </v-row>
     <v-row class="mt-4">
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
-      <v-col class="border text-center" cols="1">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
+      <v-col cols="1" class="border text-center">1</v-col>
     </v-row>
-    <v-row class="mt-4">
-      <v-col sm="12" md="4" class="border text-center">md1</v-col>
-      <v-col sm="12" md="4" class="border text-center">md2</v-col>
-      <v-col sm="12" md="4" class="border text-center">md3</v-col>
-    </v-row>
+
+    <h4 class="mt-7">반응형 레이아웃</h4>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12" md="6" lg="3" class="border text-center"
+          >column1</v-col
+        >
+        <v-col cols="12" md="6" lg="3" class="border text-center"
+          >column2</v-col
+        >
+        <v-col cols="12" md="6" lg="3" class="border text-center"
+          >column3</v-col
+        >
+        <v-col cols="12" md="6" lg="3" class="border text-center"
+          >column4</v-col
+        >
+      </v-row>
+    </v-container>
   </v-container>
 </template>
-<script setup>
-// 반응형 웹에 이용, 1280px > < 1920px : Large (small, large , medium이 기본)
-</script>
+<script setup></script>
 <style scoped></style>
